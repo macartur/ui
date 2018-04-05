f2bee3d229aa796e06bba30cbd19ae5e40997812
Macartur Sousa <macartur.sc@gmail.com>
2018-04-05 15:17:58 -0300

    Improve Topology.vue
    - change from custom_properties to metadata
    - add change link colors and clear link colors

--- src/components/kytos/topology/Topology.vue
+++ src/components/kytos/topology/Topology.vue
@@ -50,10 +50,24 @@ export default {
   },
   methods: {
     setListeners () {
+      this.$kytos.$on('changeLinksColor', this.change_links_color)
+      this.$kytos.$on('clearLinksColor', this.clear_links_color)
       this.$kytos.$on("topology-highlightAll", (p) => {
         if (!this.check_switch_under_click(p)) this.highlight_all_elements()
       })
       this.$kytos.$on("topology-toggle-label", this.toggle_labels)
+    },
+    clear_links_color(){
+      $.each(this.graph.links, function(index, link){
+          d3.select('#link-'+link.id).attr("stroke", 'gray')
+      })
+    },
+    change_links_color(color, links){
+      var self = this
+      $.each(links, function(index, link){
+          var link_id = self.fix_name(link.a) + '___' + self.fix_name(link.b)
+          d3.select('#link-'+link_id).attr("stroke", color)
+      })
     },
     check_switch_under_click (p) {
       // p: point that was clicked with p.x and p.y attributes
@@ -233,19 +247,17 @@ export default {
           node.name = node.mac
           node.id = this.fix_name(node.mac)
         } else {
-          // set the custom_properties as 'properties'
-          if (node.custom_properties) {
-            for (let prop in node.custom_properties) {
-              node[prop] = node.custom_properties[prop]
+          // fix node positions
+          if (node.metadata) {
+            for (let prop in node.metadata) {
+              node[prop] = node.metadata[prop]
             }
           }
-          // here we only have switches. Let's create the interfaces nodes.
           node.lat = null
           node.lng = null
-          // setting switch position based on its lat-lng.
-          if (node.custom_properties && node.custom_properties.lat) {
-            node.lat = node.custom_properties.lat
-            node.lng = node.custom_properties.long
+          if (node.metadata && node.metadata.lat) {
+            node.lat = node.metadata.lat
+            node.lng = node.metadata.lng
             let ll = this.project(node.lat, node.lng)
             node.x = node.fx = ll.x
             node.y = node.fy = ll.y
@@ -449,8 +461,8 @@ export default {
                .classed("iep", function(d){ return d.type == "iep" })
                .classed("switch", function(d){ return d.type == "switch" })
                .classed("interface", function(d){ return d.type == "interface" })
-               .classed("amlight", function(d) { return d.custom_properties && d.custom_properties.network && d.custom_properties.network.indexOf("Amlight") != -1 })
-               .classed("fibre", function(d) { return d.custom_properties && d.custom_properties.network && d.custom_properties.network.indexOf("Fibre") != -1 })
+               .classed("amlight", function(d) { return d.metadata && d.metadata.network && d.metadata.network.indexOf("Amlight") != -1 })
+               .classed("fibre", function(d) { return d.metadata && d.metadata.network && d.metadata.network.indexOf("Fibre") != -1 })
                .attr("r", this.gnode_radius)
                .on("click", this.highlight_switch)
 
@@ -557,9 +569,6 @@ export default {
 
     .hide
       display: none;
-
-    line
-      stroke: rgba(255, 255, 255, 0.5);
 
     .node
       cursor: pointer;
